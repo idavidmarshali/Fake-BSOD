895e5539a51594a75591d178d6f7de0514042b4d
DavidMarshal <koroshjam14@gmail.com>
2022-05-29 00:25:58 +0430

    added colors and minor changes

--- build.cmd
+++ build.cmd
@@ -1,16 +1,22 @@
 @echo off
 
-echo Building Object file ...
+echo [0;33m[INFO] Building Object file ...
 
 gcc -c -o BSOD.o BSOD.c
 
-echo linking ...
+echo [INFO] linking ...
+IF NOT EXIST build\ (
+	mkdir build
+)
+gcc -o build\BSOD.exe BSOD.o %systemroot%\System32\Kernel32.dll %systemroot%\System32\User32.dll %systemroot%\System32\Gdi32.dll %systemroot%\System32\Ole32.dll
 
-gcc -o BSOD.exe BSOD.o C:\Windows\System32\Kernel32.dll C:\Windows\System32\User32.dll C:\Windows\System32\Gdi32.dll C:\Windows\System32\Ole32.dll
-
-echo Removing object file ...
+echo [INFO] Removing object file ...
 
 del BSOD.o
+IF NOT EXIST build\qr.bmp (
+echo [INFO] Copyning Qr code image ...
+copy qr.bmp build\
+)
 
-echo Done!
+echo [0;32m[INFO] build\BSOD.exe Done![0;0m
 PAUSE
